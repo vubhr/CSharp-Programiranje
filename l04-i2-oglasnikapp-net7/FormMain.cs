a8489e600fa9a40708947a82dfe93f12833c2f41
Krunoslav Husak <khusak@vub.hr>
2024-03-26 20:37:54 +0100

    Predložak 4. lab. vj.

---
+++ l04-i2-oglasnikapp-net7/FormMain.cs
@@ -0,0 +1,13 @@
+namespace OglasnikApp {
+  public partial class FormMain : Form {
+    public FormMain() {
+      InitializeComponent();
+    }
+
+    private void btnAdd_Click(object sender, EventArgs e) {
+    }
+
+    private void RefreshAdsList() {
+    }
+  }
+}
